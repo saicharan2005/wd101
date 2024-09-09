7d7cf244d9129d05ede9982059ce3d1f790f6c59
Sai Charan Kunduru <136722766+saicharan2005@users.noreply.github.com>
2024-09-09 21:14:20 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,137 @@
+const email=document.getElementById('email');
+const dateofbirth=document.getElementById('dob');
+const submit =document.getElementById('submit');
+submit.addEventListener('click',(event)=>{
+  let isValid=true;
+  if(!(validEmail(email.value))){
+    alert("enter valid email address");
+    isValid=false;
+  }
+
+  if(!(validDateofbirth(dateofbirth.value))){
+    message="enter vaild date of birth"
+    dateofbirth.setCustomValidity(message);
+    dateofbirth.reportValidity();
+    isValid=false;
+
+
+  } else {
+    dateofbirth.setCustomValidity(""); 
+   }
+
+
+if (!isValid) {
+    event.preventDefault();
+}
+  
+ 
+});
+
+function validEmail(email){
+  const validpattern=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ return (validpattern.test(email));  
+}
+
+
+
+
+
+
+
+
+function validDateofbirth(dateofbirth){
+   const todayDate =new Date();
+   const birthDate =new Date(dateofbirth);
+
+   const age=todayDate.getFullYear() - birthDate.getFullYear();
+
+   const monthDiff=todayDate.getMonth() - birthDate.getMonth();
+
+   const dateDiff=todayDate.getDate() - birthDate.getDate();
+
+
+   if(monthDiff<0 || (monthDiff ===0 && dateDiff<0)){
+    age--;
+   }
+   
+
+   return (age>=18 && age<=55);
+   
+   
+}
+
+
+let userForm = document.getElementById("user-form");
+
+
+let userData= JSON.parse(localStorage.getItem("user-data") ) || [];
+
+
+const displayEntries =()=>{
+  const userEntries=  JSON.parse(localStorage.getItem("user-data"));
+
+
+  let tableEntries = userEntries.map(data=>{
+    const namedata =  `<td class="border px-4 py-2">${data.name}</td>`;
+    const emaildata =  `<td class="border px-4 py-2">${data.email}</td>`;
+    const passworddata =  `<td class="border px-4 py-2">${data.password}</td>`;
+    const dataofBirthdata =  `<td class="border px-4 py-2">${data.dateofBirth}</td>`;
+    const acceptTermsdata=  `<td class="border px-4 py-2">${data.acceptTerms}</td>`;
+
+
+    const rowdata =  `<tr>${namedata} ${emaildata} ${passworddata} ${dataofBirthdata} ${acceptTermsdata}</tr>`
+
+    return rowdata;
+  }).join("\n");
+
+  const tabledata =`<table class="table-auto w-fill">
+          <tr>
+            <th class="px-4 py-2">Name</th>
+            <th class="px-4 py-2">Email</th>
+            <th class="px-4 py-2">Password</th>
+            <th class="px-4 py-2">Dob</th>
+            <th class="px-4 py-2">Accepted terms?</th>
+          </tr>
+
+          ${tableEntries}
+
+        </table>`
+let userdetails = document.getElementById('user-entries');
+userdetails.innerHTML= tabledata;
+}
+
+
+
+userForm.addEventListener('submit',(event) =>{
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const email=document.getElementById('email').value;
+
+  const  password =document.getElementById('password').value;
+
+  const dateofBirth =document.getElementById('dob').value;
+
+  const acceptTerms=document.getElementById('acceptTerms').checked;
+
+
+  const entry ={
+    name,
+    email,
+    password,
+    dateofBirth,
+    acceptTerms
+  }
+
+  userData.push(entry);
+
+localStorage.setItem("user-data", JSON.stringify(userData));
+displayEntries();
+});
+
+
+displayEntries();
+
+
+
+
+
